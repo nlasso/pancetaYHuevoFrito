41107fe3a803695624b92a3ad61fe9938f184c42
Alumno de Orga2 <alumno@vmorga2.(none)>
2013-09-14 16:59:45 -0300

    ccolor en C

--- TP2-2C/tp2-bundle-v1/solucion/src/color_filter_c.c
+++ TP2-2C/tp2-bundle-v1/solucion/src/color_filter_c.c
@@ -1,11 +1,47 @@
+#include <math.h>
+#include <stdio.h>
 
-void color_filter_c(unsigned char *src,
-                    unsigned char *dst,
-                    unsigned char rc,
-                    unsigned char gc,
-                    unsigned char bc,
-                    int threshold,
-                    int width,
-                    int height)
+_Bool distancia (int rc, int gc, int bc, int r, int g, int b, int threshold){
+	int res = sqrt(pow((r - rc),2) + pow((g - gc),2) + pow((b - bc),2));
+	if(res > threshold){
+		return 1;
+	}
+	return 0;
+}
+
+
+void color_filter_c(unsigned char *src, 
+					unsigned char *dst, 
+					unsigned char rc, 
+					unsigned char gc, 
+					unsigned char bc, 
+					int threshold, 
+					int width, 
+					int height)
 {
+	unsigned char (*src_matrix)[width * 3] = (unsigned char (*)[width * 3]) src;
+	unsigned char (*dst_matrix)[width * 3] = (unsigned char (*)[width * 3]) dst;
+	int _row = 0;
+	int _column = 0;
+	while(_row <= height){
+		while(_column <= width * 3){
+			int r = src_matrix[_row][_column];
+			int g = src_matrix[_row][_column + 1];
+			int b = src_matrix[_row][_column + 2];
+			_Bool dist = distancia ((int)rc,(int)gc,(int)bc, r, g, b, threshold);
+			if(dist){
+				int rgb = (r + g + b)/3;
+				dst_matrix[_row][_column] = rgb;
+				dst_matrix[_row][_column + 1] = rgb;
+				dst_matrix[_row][_column + 2] = rgb;
+			}else{
+				dst_matrix[_row][_column] = src_matrix[_row][_column];
+				dst_matrix[_row][_column + 1] = src_matrix[_row][_column + 1];
+				dst_matrix[_row][_column + 2] = src_matrix[_row][_column + 2];
+			}
+			_column = _column + 3;
+			_row++;
+		}
+	}
+
 }
