c14b8e5dca1e27b0006b966f3ba1143248ba9d8a
nlasso <lasso.nico@gmail.com>
2013-10-10 17:29:08 -0300

    Kernel.asm ejercicio 1 terminado andando

--- TP3-2C/entregable/tp3-bundle.v1/src/kernel.asm
+++ TP3-2C/entregable/tp3-bundle.v1/src/kernel.asm
@@ -32,6 +32,20 @@ iniciando_mr_len equ    $ - iniciando_mr_msg
 iniciando_mp_msg db     'Iniciando kernel (Modo Protegido)...'
 iniciando_mp_len equ    $ - iniciando_mp_msg
 
+gdt:    dd 0x0 
+        dd 0x0 
+
+sc0: dd 0x00003FFF
+     dd 0x00C09A00
+
+sd0: dd 0x00003FFF
+     dd 0x00C09200
+
+video:  dd 0x80000FBF
+        dd 0x0040920B
+
+gdt_desc:   dw $-gdt
+            dd gdt
 ;;
 ;; Seccion de código.
 ;; -------------------------------------------------------------------------- ;;
@@ -45,15 +59,36 @@ start:
     ; Imprimir mensaje de bienvenida
     imprimir_texto_mr iniciando_mr_msg, iniciando_mr_len, 0x07, 0, 0
 
-
     ; habilitar A20
 
+    CALL habilitar_A20
+
     ; cargar la GDT
+    
+    LGDT [gdt_desc]
 
     ; setear el bit PE del registro CR0
 
+    MOV eax, cr0
+    OR eax, 1
+    MOV cr0, eax
+    JMP 0x8:modo_protegido
+
     ; pasar a modo protegido
 
+BITS 32
+
+modo_protegido:
+
+    XOR eax, eax 
+    MOV ax, 10000b
+    MOV ds, ax
+    MOV es, ax
+    MOV gs, ax
+    MOV ax, 11000b
+    MOV fs, ax
+    
+    imprimir_texto_mp iniciando_mp_msg, iniciando_mp_len, 0x07, 2, 0
     ; acomodar los segmentos
 
     ; setear la pila
