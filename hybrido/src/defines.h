2e173e30ceb886f2f29f0268259b26e477b95f4a
ubuntu-scar64 <scar@ubuntu.(none)>
2013-11-02 05:08:49 -0300

    Despues mira tu casilla de mail, hay explico todo

---
+++ hybrido/src/defines.h
@@ -0,0 +1,119 @@
+/* ** por compatibilidad se omiten tildes **
+================================================================================
+ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+================================================================================
+
+    Definiciones globales del sistema.
+*/
+
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+/* Bool */
+/* -------------------------------------------------------------------------- */
+#define TRUE                    0x00000001
+#define FALSE                   0x00000000
+
+
+/* Graficos */
+/* -------------------------------------------------------------------------- */
+#define SCREENESTADO			0
+#define SCREENMAPA				1
+#define ASCII_first_num			48
+#define ASCII_space				0x20
+#define NAMEGRUPO				"Palito Peron Helado/Best Name Ever"
+#define TABLAERRORES_Y			2
+#define TABLAERRORES_X			49
+#define TABLAERRORES_ANCHO		29
+#define TABLAERRORES_ALTO		14
+
+/* Misc */
+/* -------------------------------------------------------------------------- */
+#define CANT_TAREAS             8
+#define TAMANO_PAGINA           0x00001000
+#define CANT_ENTRADAS          	0x00000400
+#define CANT_IDENTITY_MAPEOS	0x00000780 //ESTO PODRIA SER 7FF, REVISAR
+#define TASK_SIZE               2 * 4096
+
+
+/* Indices en la gdt */
+/* -------------------------------------------------------------------------- */
+#define GDT_IDX_NULL_DESC           0
+#define GDT_IDX_CODE_0				18
+#define GDT_IDX_CODE_3				19
+#define GDT_IDX_DATA_0				20
+#define GDT_IDX_DATA_3				21
+#define GDT_IDX_VIDEO				22
+#define GDT_TSS_IDLE				23
+#define GDT_TSS_TS1					24
+#define GDT_TSS_TS2					25
+#define GDT_TSS_TS3					26
+#define GDT_TSS_TS4					27
+#define GDT_TSS_TS5					28
+#define GDT_TSS_TS6					29
+#define GDT_TSS_TS7					30
+#define GDT_TSS_TS8					31
+
+/* Direcciones de memoria */
+/* -------------------------------------------------------------------------- */
+#define BOOTSECTOR              0x00001000 /* direccion fisica de comienzo del bootsector (copiado) */
+#define KERNEL                  0x00001200 /* direccion fisica de comienzo del kernel */
+#define VIDEO                   0x000B8000 /* direccion fisica del buffer de video */
+#define MEMVIDEO				0x000A0000
+
+#define PILAKERNEL				0X00026000
+#define PILALVLCERO	 			0x00029000  
+
+#define BUFFERESTADO			0X0002E000
+#define BUFFERMAPA				0X0002F000
+
+#define MAINPAGEDIR 			0x00027000   /* direccion fisica del primer page dir */
+#define FIRSTPAGETAB 			0x00028000   /* direccion fisica del primer page tab */
+#define SECONDPAGETAB 			0x00030000   /* direccion fisica del segundo page tab */
+
+#define SECTORFREEMEM 			0x00031000   /* direccion fisica del primer page dir */
+
+
+/* Direcciones virtuales de código, pila y datos */
+/* -------------------------------------------------------------------------- */
+#define TASK_CODE               0x40000000 /* direccion virtual del codigo */
+
+//#define TASK_IDLE_CODE          0x40000000 /* direccion virtual del codigo de la tarea idle */
+//#define TASK_IDLE_STACK         0x003D0000 /* direccion virtual de la pila de la tarea idle */
+
+#define TASK_ANCLA              0x40002000
+#define TASK_ANCLA_FIS          0x00000000
+
+#define AREA_TIERRA_INICIO      0x00000000  /* 0.0 MB     */
+#define AREA_TIERRA_FIN         0x000FFFFF  /* 1.0 MB - 1 */
+#define AREA_MAR_INICIO         0x00100000  /* 1.0 MB     */
+#define AREA_MAR_FIN            0x0077FFFF  /* 7.5 MB - 1 */
+
+/* Direcciones fisicas de codigos */
+/* -------------------------------------------------------------------------- */
+/* En estas direcciones estan los códigos de todas las tareas. De aqui se
+ * copiaran al destino indicado por TASK_<i>_CODE_ADDR.
+ */
+#define TASK_1_CODE_SRC_ADDR    0x00010000
+#define TASK_2_CODE_SRC_ADDR    0x00012000
+#define TASK_3_CODE_SRC_ADDR    0x00014000
+#define TASK_4_CODE_SRC_ADDR    0x00016000
+#define TASK_5_CODE_SRC_ADDR    0x00018000
+#define TASK_6_CODE_SRC_ADDR    0x0001A000
+#define TASK_7_CODE_SRC_ADDR    0x0001C000
+#define TASK_8_CODE_SRC_ADDR    0x0001E000
+
+#define TASK_IDLE_CODE_SRC_ADDR 0x00020000
+
+/* INTERRUPCIONES */
+#define INTCLOCK 31 	//NOTA: ESTO ESTA MAL !!!!!!!
+#define INTKEYBOARD 32 	//NOTA: ESTO ESTA MAL !!!!!!!
+#define INTSERVICIOS 50
+	 //Definiciones de int servicios 
+	#define ANCLA 	0x923
+	#define MISIL 	0x83A
+	#define NAVEGAR 0xAEF
+#define INTBANDERA 66
+////
+
+#endif  /* !__DEFINES_H__ */
---
+++ hybrido/src/isr.asm
@@ -0,0 +1,97 @@
+; ** por compatibilidad se omiten tildes **
+; ==============================================================================
+; TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+; ==============================================================================
+; definicion de rutinas de atencion de interrupciones
+
+%include "imprimir.mac"
+
+BITS 32
+
+
+;; PIC
+extern fin_intr_pic1
+extern print_error
+
+
+;;
+;; Definición de MACROS
+;; -------------------------------------------------------------------------- ;;
+
+%macro ISR 1
+global _isr%1
+
+_isr%1:
+.loopear:
+    push word %1
+    call print_error
+    pop  word ax
+    ; To Infinity And Beyond!!
+    ;jmp $
+%endmacro
+
+;;
+;; Datos
+;; -------------------------------------------------------------------------- ;;
+; Scheduler
+reloj_numero:           dd 0x00000000
+reloj:                  db '|/-\'
+
+
+;;
+;; Rutina de atención de las EXCEPCIONES
+;; -------------------------------------------------------------------------- ;;
+ISR 0
+ISR 1
+ISR 2
+ISR 3
+ISR 4
+ISR 5
+ISR 6
+ISR 7
+ISR 8
+ISR 9
+ISR 10
+ISR 11
+ISR 12
+ISR 13
+ISR 14
+ISR 15
+ISR 16
+ISR 17
+ISR 18
+ISR 19
+ISR 31
+ISR 32
+ISR 50
+ISR 66
+
+;;
+;; Rutina de atención del RELOJ
+;; -------------------------------------------------------------------------- ;;
+
+;;
+;; Rutina de atención del TECLADO
+;; -------------------------------------------------------------------------- ;;
+
+;;
+;; Rutinas de atención de las SYSCALLS
+;; -------------------------------------------------------------------------- ;;
+
+;; Funciones Auxiliares
+;; -------------------------------------------------------------------------- ;;
+proximo_reloj:
+    pushad
+
+    inc DWORD [reloj_numero]
+    mov ebx, [reloj]
+    cmp ebx, 0x4
+    jl .ok
+        mov DWORD [reloj_numero], 0x0
+        mov ebx, 0
+    .ok:
+        add ebx, reloj
+        imprimir_texto_mp ebx, 1, 0x0f, 24, 79
+
+    popad
+    ret
